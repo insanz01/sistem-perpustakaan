b992a387f19182cc30366a3ec4dc2198c0b72cc0
insanz01 <insankamil002@gmail.com>
2023-01-24 00:18:40 +0700

    fix path while login

--- application/config/routes.php
+++ application/config/routes.php
@@ -56,7 +56,7 @@ $route['translate_uri_dashes'] = FALSE;
 $route['auth'] = "AuthController/index";
 // $ruote['auth/login'] = "AuthController/index";
 // $ruote['login'] = "AuthController/index";
-$ruote['auth/do_login'] = "AuthController/do_login";  
+// $ruote['auth/do_login'] = "AuthController/do_login";  
 $ruote['do_login'] = "AuthController/do_login";
 
 $route['dashboard'] = 'AppController/index';
