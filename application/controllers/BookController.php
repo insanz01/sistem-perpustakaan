1841e60a9b71a60e25c6287c33f3f075dc64f84f
insanz01 <insankamil002@gmail.com>
2023-01-30 19:13:13 +0700

    add filetype support for jpeg

--- application/controllers/BookController.php
+++ application/controllers/BookController.php
@@ -55,7 +55,7 @@ class BookController extends CI_Controller
     ];
 
     $config['upload_path']          = './uploads/';
-    $config['allowed_types']        = 'gif|jpg|png';
+    $config['allowed_types']        = 'gif|jpg|png|jpeg';
     // $config['max_size']             = 100;
     // $config['max_width']            = 1024;
     // $config['max_height']           = 768;
