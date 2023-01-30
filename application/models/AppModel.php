66ce9ac29bb50f626b7f157f4b8c7ebfeb6d167f
insanz01 <insankamil002@gmail.com>
2023-01-30 19:26:56 +0700

    fix jumlah pengunjung

--- application/models/AppModel.php
+++ application/models/AppModel.php
@@ -18,20 +18,20 @@ class AppModel extends CI_Model {
   }
 
   public function total_visitor_today() {
-    $query = "SELECT * FROM bukutamu WHERE created_at = CURDATE()";
+    $query = "SELECT * FROM bukutamu WHERE DATE(created_at) = CURDATE()";
     $visitors = $this->db->query($query)->result_array();
 
-    $queryMember = "SELECT * FROM bukutamu_membership WHERE created_at = CURDATE()";
+    $queryMember = "SELECT * FROM bukutamu_membership WHERE DATE(created_at) = CURDATE()";
     $member_visitors = $this->db->query($query)->result_array();
 
     return count($visitors) + count($member_visitors);
   }
 
   public function total_visitor_week() {
-    $query = "SELECT * FROM bukutamu WHERE created_at = CURDATE()";
+    $query = "SELECT * FROM bukutamu WHERE WEEK(created_at) = WEEK(CURDATE())";
     $visitors = $this->db->query($query)->result_array();
 
-    $queryMember = "SELECT * FROM bukutamu_membership WHERE created_at = CURDATE()";
+    $queryMember = "SELECT * FROM bukutamu_membership WHERE WEEK(created_at) = WEEK(CURDATE())";
     $member_visitors = $this->db->query($query)->result_array();
 
     return count($visitors) + count($member_visitors);
